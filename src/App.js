d0bb8b3af0bbe4ed3752d44b4360f71844e795db
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-15 09:39:32 +0200

    Add CSS grid.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import { PDFViewer } from '@react-pdf/renderer';
 import PDF from './components/PDF'
+import styles from './App.css';
 
-export default class App extends React.Component{
+export default class App extends React.Component {
   render() {
     return (
-     <PDFViewer>
-      <PDF/>
-    </PDFViewer >
-  )}
+      <div className="container">
+        <div className="ui"></div>
+        <PDFViewer style={styles.pdf} className="pdf">
+          <PDF />
+        </PDFViewer >
+      </div>
+      
+    )
+  }
 }
 
 render(<App />, document.getElementById('root'));
