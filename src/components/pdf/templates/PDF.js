daf8efbea078278efcc132bab8a823dc4301ab8c
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-16 02:18:26 +0200

    Rename sections.

--- src/components/pdf/templates/PDF.js
+++ src/components/pdf/templates/PDF.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
-import CustomSection from '../../sections/CustomSection'
-import CertifiactesSection from '../../sections/CertificatesSection'
-import Header from '../Header'
+import CustomSection from '../../sections/Custom'
+import CertifiactesSection from '../../sections/Certificates'
+import Header from '../../sections/Header'
 
 export const styles = StyleSheet.create({
   page: {
