78f31e305a4d3769954b26dfef27ce37fbb7f8d5
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-20 03:42:05 +0200

    Dynamically preview PDF.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { render } from 'react-dom';
-import ReactDOM from 'react-dom';
-import { PDFViewer } from '@react-pdf/renderer';
 import PDF from './components/pdf/templates/PDF'
 import styles from './App.css';
 import UI from './components/ui/UI'
-import ReactPDF from '@react-pdf/renderer';
+import { pdfjs } from 'react-pdf'
+import { pdf } from '@react-pdf/renderer'
+import { Document, Page } from 'react-pdf'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      base64: '',
+      blob: null
+    }
+    this.getBase64 = this.getBase64.bind(this);
+  }
+
+  getBase64(template) {
+    var reader = new FileReader();
+    pdf(template).toBlob().then((blob) => {
+      this.blob = blob
+      reader.readAsDataURL(blob);
+      reader.onloadend = () => {
+        this.setState({
+          base64: reader.result
+        })
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.getBase64(<PDF />)
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.base64)
+  }
+
   render() {
     return (
       <div className="container">
@@ -14,9 +46,9 @@ export default class App extends React.Component {
           <UI />
         </div>
         <div className="pdf">
-          <PDFViewer style={styles.pdf} className="pdf">
-            <PDF />
-          </PDFViewer >
+          <Document file={`${this.state.base64}`}>
+            <Page pageNumber={1} />
+          </Document>
         </div>
       </div>
 
@@ -24,4 +56,3 @@ export default class App extends React.Component {
   }
 }
 
-ReactPDF.render(<App />, `${__dirname}/example.pdf`);
