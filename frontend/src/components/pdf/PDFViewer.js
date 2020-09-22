60b174db9f6a5d4fc740141fece6eb40caa38c4f
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-22 04:29:08 +0200

    Add pagination to PDFViewer.

--- frontend/src/components/pdf/PDFViewer.js
+++ frontend/src/components/pdf/PDFViewer.js
@@ -3,6 +3,8 @@ import PDF from './templates/PDF'
 import { pdf } from '@react-pdf/renderer'
 import { Document, Page, pdfjs } from 'react-pdf'
 import styles from '../../styles/PDFViewer.css'
+import { Button } from 'semantic-ui-react'
+
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default class PDFViewer extends React.Component {
@@ -10,8 +12,17 @@ export default class PDFViewer extends React.Component {
     super(props)
     this.state = {
       blob: null,
+      activePage: 1,
+      numberOfPages: 0,
+      isNextPage: false,
+      isPreviousPage: false
     }
+
+    this.setIsNextPage = this.setIsNextPage.bind(this)
+    this.setIsPreviousPage = this.setIsPreviousPage.bind(this)
+    this.setNumberOfPages = this.setNumberOfPages.bind(this)
   }
+
 
   getBlob(template) {
     pdf(template).toBlob().then((blob) => {
@@ -27,13 +38,50 @@ export default class PDFViewer extends React.Component {
   componentDidMount() {
     this.getBlob(this.props.template)
   }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.numberOfPages != this.state.numberOfPages || prevState.activePage != this.state.activePage) {
+      this.setState({
+        isNextPage: this.isNextPage(),
+        isPreviousPage: this.isPreviousPage()
+      })
+    }
+  }
 
+  setIsNextPage() {
+    const activePage = this.state.activePage
+    this.setState({
+      activePage: this.isNextPage() ? activePage + 1 : activePage
+    })
+  }
+
+  setIsPreviousPage() {
+    this.setState({
+      activePage: this.isPreviousPage() ? this.state.activePage -1 : this.state.activePage
+    })
+  }
+
+  isNextPage() {
+    return this.state.activePage < this.state.numberOfPages
+  }
+
+  isPreviousPage() {
+    return this.state.activePage > 1
+  }
+
+  setNumberOfPages(pdf) {
+    this.setState({
+      numberOfPages: pdf.numPages
+    })
+  }
 
   render() {
     return (
+
       <div className="pdf" style={styles.pdf}>
-        <Document file={`${this.state.blob}`} renderMode="svg" >
-          <Page pageNumber={1} />
+        <Button onClick={this.setIsPreviousPage} className={`${this.state.isPreviousPage ? 'enabled' : 'disabled'}`}>left</Button>
+        <Button onClick={this.setIsNextPage} className={`${this.state.isNextPage ? 'enabled' : 'disabled'}`}>right</Button>
+        <Document file={`${this.state.blob}`} renderMode="svg" onLoadSuccess={this.setNumberOfPages}>
+          <Page pageNumber={this.state.activePage} />
         </Document>
       </div>
     )
