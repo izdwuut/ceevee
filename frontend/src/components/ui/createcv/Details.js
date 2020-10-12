1fe97f98e67e50ec3b947114d0f24a31d93fd9ec
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-10-12 08:37:59 +0200

    Style Details UI component.

--- frontend/src/components/ui/createcv/Details.js
+++ frontend/src/components/ui/createcv/Details.js
@@ -1,16 +1,31 @@
 import MainContext from '../../../CreateCVApp';
 import * as React from 'react';
-import { Form, Segment, Header } from 'semantic-ui-react'
 import { updateFirstName, updateMiddleName, updateLastName, updatePosition, updateEmail, updateMobile, updateCountry, updateCity, updateDrivingLicense, updateBirthDate } from '../../../redux/reducers/ui/metaData/actions'
 import { updatePreview } from '../../../redux/reducers/pdf/pdfViewer/actions'
 import { connect } from "react-redux";
-import {debounceTime} from '../../../utilities/variables'
+import { debounceTime } from '../../../utilities/variables'
 import debounce from '../../../utilities/debounce'
+import * as UI from '../../../utilities/ui'
+
+import {
+    Icon,
+    InputIcon,
+    Button,
+    Card,
+    Input,
+    Accordion,
+    AccordionPanel,
+    Tooltip,
+    Textarea,
+    CardEmpty
+} from '@salesforce/design-system-react';
 
 export class Details extends React.Component {
     constructor(props) {
-        super(props)
-
+        super(props);
+        this.state = {
+            expandedPanels: {},
+        }
     }
 
     updatePreview = debounce(() => {
@@ -59,73 +74,110 @@ export class Details extends React.Component {
         this.updatePreview()
     }
     render() {
-        return (
-            <Segment>
-            <Header>{this.props.header}</Header>
-            <Form>
 
-                <Form.Input
-                    label='Name'
-                    type='text'
-                    onChange={e => this.updateFirstName(e)}
-                    value={this.props.firstName}
-                />
-                <Form.Input
-                    label='Middle name'
-                    type='text'
-                    onChange={e => this.updateMiddleName(e)}
-                    value={this.props.middleName}
-                />
-                <Form.Input
-                    label='Last name'
-                    type='text'
-                    onChange={e => this.updateLastName(e)}
-                    value={this.props.lastName}
-                />
-                <Form.Input
-                    label='Position'
-                    type='text'
-                    onChange={e => this.updatePosition(e)}
-                    value={this.props.position}
-                />
-                <Form.Input
-                    label='Email'
-                    type='text'
-                    onChange={e => this.updateEmail(e)}
-                    value={this.props.email}
-                />
-                <Form.Input
-                    label='Mobile'
-                    type='text'
-                    onChange={e => this.updateMobile(e)}
-                    value={this.props.mobile}
-                />
-                <Form.Input
-                    label='Country'
-                    type='text'
-                    onChange={e => this.updateCountry(e)}
-                    value={this.props.country}
-                />
-                <Form.Input
-                    label='City'
-                    type='text'
-                    onChange={e => this.updateCity(e)}
-                    value={this.props.city}
-                />
-                <Form.Input
-                    label='Driving license'
-                    type='text'
-                    onChange={e => this.updateDrivingLicense(e)}
-                    value={this.props.drivingLicense}
-                />
-                <Form.Input
-                    label='Birth Date'
-                    type='text'
-                    onChange={e => this.updateBirthDate(e)}
-                    value={this.props.birthDate}
-                />
-            </Form>
-            </Segment>
+        return (
+            <Card
+                heading="Details"
+                icon={<Icon category="standard" name="contact" size="small" />}
+            >
+                <div className="slds-grid slds-gutters slds-col_padded">
+                    <div className="slds-col">
+                        <Input
+                            variant="outlined"
+                            label='First name'
+                            onChange={e => this.updateFirstName(e)}
+                            value={this.props.firstName}
+                        />
+                    </div>
+                    <div className="slds-col">
+                        <Input
+                            variant="outlined"
+                            label='Middle name'
+                            onChange={e => this.updateMiddleName(e)}
+                            value={this.props.middleName}
+                        />
+
+                    </div>
+                </div>
+                <div className="slds-grid slds-gutters slds-col_padded">
+                    <div className="slds-col">
+
+                        <Input
+                            variant="outlined"
+                            label='Last name'
+                            onChange={e => this.updateLastName(e)}
+                            value={this.props.lastName}
+                        />
+                    </div>
+                    <div className="slds-col">
+                        <Input
+                            variant="outlined"
+                            label='Email'
+                            onChange={e => this.updateEmail(e)}
+                            value={this.props.email}
+                        />
+                    </div>
+                </div>
+                <div className="slds-grid slds-gutters slds-col_padded">
+                    <div className="slds-col">
+
+                        <Input
+                            variant="outlined"
+                            label='Position'
+                            onChange={e => this.updatePosition(e)}
+                            value={this.props.position}
+                        />
+                    </div>
+                    <div className="slds-col">
+                        <Input
+                            variant="outlined"
+                            label='Mobile'
+                            onChange={e => this.updateMobile(e)}
+                            value={this.props.mobile}
+                        />
+                    </div>
+                </div>
+                <div className="slds-grid slds-gutters slds-col_padded">
+                    <div className="slds-col">
+
+                        <Input
+                            variant="outlined"
+                            label='Country'
+                            onChange={e => this.updateCountry(e)}
+                            value={this.props.country}
+                        />
+
+                    </div>
+                    <div className="slds-col">
+                        <Input
+                            variant="outlined"
+                            label='City'
+                            onChange={e => this.updateCity(e)}
+                            value={this.props.city}
+                        />
+                    </div>
+                </div>
+                <div className="slds-grid slds-gutters slds-col_padded">
+                    <div className="slds-col">
+
+                        <Input
+                            variant="outlined"
+                            label='Driving license'
+                            onChange={e => this.updateDrivingLicense(e)}
+                            value={this.props.drivingLicense}
+                        />
+
+                    </div>
+                    <div className="slds-col">
+                        <Input
+                            variant="outlined"
+                            label='Birth Date'
+                            onChange={e => this.updateBirthDate(e)}
+                            value={this.props.birthDate}
+                        />
+                    </div>
+                </div>
+            </Card>
         )
     }
 }
