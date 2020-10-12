bcd7f1ed10ef0dcccda5bc04171989995f93f725
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-10-12 13:25:09 +0200

    Style GDPA UI component.

--- frontend/src/components/ui/createcv/GDPA.js
+++ frontend/src/components/ui/createcv/GDPA.js
@@ -1,37 +1,54 @@
 import MainContext from '../../../CreateCVApp';
 import * as React from 'react';
-import { Form, Segment, Header, TextArea } from 'semantic-ui-react'
 import { updateGdpa } from '../../../redux/reducers/ui/gdpa/actions'
 import { updatePreview } from '../../../redux/reducers/pdf/pdfViewer/actions'
 import { connect } from "react-redux";
 import { debounceTime } from '../../../utilities/variables'
 import debounce from '../../../utilities/debounce'
+import * as UI from '../../../utilities/ui'
 
+import {
+    Icon,
+    InputIcon,
+    Button,
+    Card,
+    Input,
+    Accordion,
+    AccordionPanel,
+    Tooltip,
+    Textarea,
+    CardEmpty
+} from '@salesforce/design-system-react';
 export class GDPA extends React.Component {
     updatePreview = debounce(() => {
         this.props.updatePreview(true)
     }, debounceTime)
 
-    updateGdpa = (e) => {
-        this.props.updateGdpa(e)
+    updateGdpa = (gdpa) => {
+        this.props.updateGdpa(gdpa)
         this.updatePreview()
     }
     render() {
         return (
-            <Segment>
-                <Header>{this.props.header}</Header>
-                <p>
+            <Card
+                heading="GDPA"
+
+                icon={<Icon category="standard" name="note" size="small" />}
+            >
+                <p className='slds-col_padded'>
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
                 </p>
-                <Form>
-                    <Form.Field
-                        control={TextArea}
-                        label='Description'
-                        value={this.props.gdpa}
-                        onChange={e => this.updateGdpa(e.target.value)}
-                    />
-                </Form>
-            </Segment>
+                <div className="slds-grid slds-gutters slds-col_padded">
+                    <div className="slds-col">
+                        <Textarea
+
+                            label='Description'
+                            value={this.props.gdpa}
+                            onChange={e => this.updateGdpa(e.target.value)}
+                        />
+                    </div>
+                </div>
+            </Card>
         )
     }
 }
--- frontend/src/redux/reducers/ui/gdpa/gdpa.js
+++ frontend/src/redux/reducers/ui/gdpa/gdpa.js
@@ -4,7 +4,7 @@ const initialState = {
     header: 'GDPA',
     gdpa: 'I agree to the processing of personal data provided in this document for realising the recruitment process pursuant to the Personal Data Protection Act of 10 May 2018 (Journal of Laws 2018, item 1000) and in agreement with Regulation (EU) 2016/679 of the European Parliament and of the Council of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation).',
     originalGdpa: 'I agree to the processing of personal data provided in this document for realising the recruitment process pursuant to the Personal Data Protection Act of 10 May 2018 (Journal of Laws 2018, item 1000) and in agreement with Regulation (EU) 2016/679 of the European Parliament and of the Council of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation).',
-    visible: false
+    visible: true
 }
 
 export default function gdpa(state = initialState, action) {
@@ -12,8 +12,8 @@ export default function gdpa(state = initialState, action) {
         case actions.GDPA_UPDATE_GDPA: {
             return {
                 ...state,
-                gdpa: action.gdpa,
-                visible: gdpa !== ''
+                gdpa: action.payload.gdpa,
+                visible: action.payload.gdpa !== ''
             }
         }
         default:
