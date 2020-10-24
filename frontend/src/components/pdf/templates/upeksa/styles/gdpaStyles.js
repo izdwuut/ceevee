148c1bb1cd28ed2c6721f5da1a7b28999d805819
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-10-24 21:39:37 +0200

    Add paddings to Education and Experience PDF components.

--- frontend/src/components/pdf/templates/upeksa/styles/gdpaStyles.js
+++ frontend/src/components/pdf/templates/upeksa/styles/gdpaStyles.js
@@ -2,7 +2,4 @@ import { StyleSheet } from '@react-pdf/renderer';
 import * as Variables from './variables'
 
 export const gdpaStyles = StyleSheet.create({
-    gdpa: {
-        paddingTop: Variables.contentsPadding
-    }
 })
