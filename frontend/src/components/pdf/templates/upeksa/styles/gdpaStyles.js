a6b6f034316b6d0c15dc59d9f3702a2e6491f5f9
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-10-24 22:33:10 +0200

    Resize spacings in GDPA PDF component.

--- frontend/src/components/pdf/templates/upeksa/styles/gdpaStyles.js
+++ frontend/src/components/pdf/templates/upeksa/styles/gdpaStyles.js
@@ -2,4 +2,7 @@ import { StyleSheet } from '@react-pdf/renderer';
 import * as Variables from './variables'
 
 export const gdpaStyles = StyleSheet.create({
+    gdpa: {
+            paddingTop: Variables.sectionPadding,
+    }
 })
