62fa5df040a74e7e730debcbbfe98cc4b76dfa54
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-10-24 21:22:48 +0200

    Style GDPA PDF component.

--- frontend/src/components/pdf/templates/upeksa/styles/gdpaStyles.js
+++ frontend/src/components/pdf/templates/upeksa/styles/gdpaStyles.js
@@ -1,4 +1,8 @@
 import { StyleSheet } from '@react-pdf/renderer';
+import * as Variables from './variables'
 
 export const gdpaStyles = StyleSheet.create({
+    gdpa: {
+        paddingTop: Variables.contentsHeaderPadding
+    }
 })
