84eacc21d264190ecb1d98e61465bed76399ac43
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-01 22:14:07 +0100

    Add header to CVList view.

--- frontend/src/pages/ui/cvlist/template/CVList.js
+++ frontend/src/pages/ui/cvlist/template/CVList.js
@@ -2,58 +2,75 @@ import * as React from 'react';
 import MainContext from '../../../../index';
 
 import {
-    Icon,
-    InputIcon,
-    Button,
-    Card,
-    Input,
-    Accordion,
-    AccordionPanel,
-    Tooltip,
-    Textarea,
-    CardEmpty,
-    PageHeader,
-    
+  Icon,
+  InputIcon,
+  Button,
+  Card,
+  Input,
+  Accordion,
+  AccordionPanel,
+  Tooltip,
+  Textarea,
+  CardEmpty,
+  PageHeader,
+  PageHeaderControl
+
 } from '@salesforce/design-system-react';
 import { connect } from 'react-redux'
 import Header from '../../components/header/Header'
-
-
+import './CVList.css'
+import { getAdd } from '../../../../utilities/ui'
 export default class CVList extends React.Component {
   render() {
 
     return (
       <div className="container">
         <Header />
-        <div className="ui-container">
-        <PageHeader
-					icon={
-						<Icon
-							assistiveText={{
-								label: 'CV',
-							}}
-							category="doctype"
-							name="unknown"
-							style={{
-								
-								fill: '#ffffff',
-							}}
-							title="CV"
-						/>
-          }
-          className="page-header"
-					title="Create CV"
-          info={<div className="description">
-            Our core functionality - creating CVs. Here you have access to various components, like your personal details, work experience, education, and more. When something is not being used, it doesn't display on a generated document. By default, the preview is pretty much empty. That's because it fills with content as you update the form.
-            <br /><br />
-            You have an option to download the generated document in two formats: PDF and text (for accessibility).
-          </div>}
-					truncate
-					variant="object-home"
-				/>
-        </div>
+        <div class="contents">
+          <PageHeader
+            icon={
+              <Icon
+                assistiveText={{
+                  label: 'CV',
+                }}
+                category="doctype"
+                name="unknown"
+                style={{
 
+                  fill: '#ffffff',
+                }}
+                title="CV"
+              />
+            }
+            className="page-header"
+            title="CV List"
+            info={<div className="description">
+              A list of CVs you've created. You can edit title, go to editing, clone, delete and download the documents.
+              <br /><br />
+              Next to each CV is a graphical representation of a PDF to be created. Because of it's size, it's a bit undecipherable, but we believe it's enough for a sneak peak. Once opened, the CV editing view provides a preview with more detail.
+          </div>}
+            truncate
+            variant="object-home"
+            onRenderActions={() => {
+              return (
+                <PageHeaderControl>
+
+                  <Button
+                    label="Create new CV"
+                    iconCategory="utility"
+                    iconName="add"
+                    iconPosition="left"
+                  />
+
+                </PageHeaderControl>
+              )
+            }
+
+            }
+          />
+        </div>
       </div>
+
     )
   }
 }
