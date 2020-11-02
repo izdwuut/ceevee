4b7a9b178f01f23dbe95c38be83ee4a39a864da8
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-02 10:47:22 +0100

    Use Emotion in CreateCV.

--- frontend/src/pages/ui/createcv/template/CreateCV.js
+++ frontend/src/pages/ui/createcv/template/CreateCV.js
@@ -1,6 +1,8 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 import * as React from 'react';
 import PDFViewer from '../../../pdf/PDFViewer/PDFViewer'
-import './CreateCV.css'
+import styles from './CreateCV.css'
 import Skills from '../Skills'
 import Details from '../Details';
 import Hobbies from '../Hobbies';
@@ -16,18 +18,18 @@ import { SizeMe } from 'react-sizeme'
 import Header from '../../components/header/Header'
 
 import {
-    Icon,
-    InputIcon,
-    Button,
-    Card,
-    Input,
-    Accordion,
-    AccordionPanel,
-    Tooltip,
-    Textarea,
-    CardEmpty,
-    PageHeader,
-    
+  Icon,
+  InputIcon,
+  Button,
+  Card,
+  Input,
+  Accordion,
+  AccordionPanel,
+  Tooltip,
+  Textarea,
+  CardEmpty,
+  PageHeader,
+
 } from '@salesforce/design-system-react';
 import { connect } from 'react-redux'
 
@@ -36,34 +38,36 @@ export default class CreateCV extends React.Component {
   render() {
 
     return (
-      <div className="container">
+      <div className="container" styles={css`
+    ${styles}
+    `}>
         <Header />
         <div className="ui-container">
-        <PageHeader
-					icon={
-						<Icon
-							assistiveText={{
-								label: 'CV',
-							}}
-							category="doctype"
-							name="unknown"
-							style={{
-								
-								fill: '#ffffff',
-							}}
-							title="CV"
-						/>
-          }
-          className="page-header"
-					title="Create CV"
-          info={<div className="description">
-            Our core functionality - creating CVs. Here you have access to various components, like your personal details, work experience, education, and more. When something is not being used, it doesn't display on a generated document. By default, the preview is pretty much empty. That's because it fills with content as you update the form.
+          <PageHeader
+            icon={
+              <Icon
+                assistiveText={{
+                  label: 'CV',
+                }}
+                category="doctype"
+                name="unknown"
+                style={{
+
+                  fill: '#ffffff',
+                }}
+                title="CV"
+              />
+            }
+            className="page-header"
+            title="Create CV"
+            info={<div className="description">
+              Our core functionality - creating CVs. Here you have access to various components, like your personal details, work experience, education, and more. When something is not being used, it doesn't display on a generated document. By default, the preview is pretty much empty. That's because it fills with content as you update the form.
             <br /><br />
             You have an option to download the generated document in two formats: PDF and text (for accessibility).
           </div>}
-					truncate
-					variant="object-home"
-				/>
+            truncate
+            variant="object-home"
+          />
           <Details context={MainContext} />
           <Experience context={MainContext} />
           <Education context={MainContext} />
