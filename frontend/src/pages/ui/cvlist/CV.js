36e4790647ea22ce0c93271f049abfca425b5204
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-02 10:07:10 +0100

    Add Download dropdown.

---
+++ frontend/src/pages/ui/cvlist/CV.js
@@ -0,0 +1,87 @@
+import MainContext from '../../../index';
+import * as React from 'react';
+
+import { updatePreview } from '../../../redux/reducers/pdf/pdfViewer/actions'
+import { connect } from "react-redux";
+import { debounceTime } from '../../../utilities/variables'
+import debounce from '../../../utilities/debounce'
+import * as UI from '../../../utilities/ui'
+
+import {
+    Dropdown,
+    Icon,
+    InputIcon,
+    Button,
+    Card,
+    Input,
+    Accordion,
+    AccordionPanel,
+    Tooltip,
+    Textarea,
+    CardEmpty
+} from '@salesforce/design-system-react';
+export class CV extends React.Component {
+
+    updatePreview = debounce(() => {
+        this.props.updatePreview(true)
+    }, debounceTime)
+
+    updateGdpa = (gdpa) => {
+        this.props.updateGdpa(gdpa)
+        this.updatePreview()
+    }
+    render() {
+        return (
+            <Card
+                heading={this.props.header}
+
+                icon={<Icon category="doctype" name="unknown" size="small" />}
+            >
+                <div class="slds-grid slds-wrap">
+                    <div class="slds-col slds-size_6-of-12">
+                        <img src="/preview-icon.png" alt="CV preview" className="cv-preview" />
+                    </div>
+                    {/* float right */}
+                    <div class="slds-col cv-actions slds-size_6-of-12">
+                        <Button
+                            label="Rename CV"
+                            iconCategory="utility"
+                            iconName="edit"
+                            iconPosition="left"
+                        />
+                        <Button
+                            label="Edit CV"
+                            iconCategory="utility"
+                            iconName="edit_form"
+                            iconPosition="left"
+                        />
+                        <Button
+                            label="Clone CV"
+                            iconCategory="utility"
+                            iconName="copy"
+                            iconPosition="left"
+                        />
+                        <Dropdown
+                            align="right"
+                            iconCategory="utility"
+                            iconName="download"
+                            iconPosition="left"
+                            label="Download..."
+                            options={[
+                                { label: 'PDF', value: 'pdf' },
+                                { label: 'Text', value: 'text' },
+                            ]}
+                        />
+                        <Button
+                            label="Delete CV"
+                            iconCategory="utility"
+                            iconName="delete"
+                            iconPosition="left"
+                            variant="destructive"
+                        />
+                    </div>
+                </div>
+            </Card>
+        )
+    }
+}
--- frontend/src/pages/ui/cvlist/template/CVList.js
+++ frontend/src/pages/ui/cvlist/template/CVList.js
@@ -20,54 +20,73 @@ import { connect } from 'react-redux'
 import Header from '../../components/header/Header'
 import './CVList.css'
 import { getAdd } from '../../../../utilities/ui'
+import { CV } from '../CV'
 export default class CVList extends React.Component {
   render() {
 
     return (
       <div className="container">
         <Header />
-        <div class="contents">
-          <PageHeader
-            icon={
-              <Icon
-                assistiveText={{
-                  label: 'CV',
-                }}
-                category="doctype"
-                name="unknown"
-                style={{
 
-                  fill: '#ffffff',
-                }}
-                title="CV"
-              />
-            }
-            className="page-header"
-            title="CV List"
-            info={<div className="description">
-              A list of CVs you've created. You can edit title, go to editing, clone, delete and download the documents.
+        <PageHeader
+
+          icon={
+            <Icon
+              assistiveText={{
+                label: 'CV',
+              }}
+              category="standard"
+              name="picklist_type"
+              style={{
+
+                fill: '#ffffff',
+              }}
+              title="CV"
+            />
+          }
+          className="page-header"
+          title="CV List"
+          info={<div className="description">
+            A list of CVs you've created. You can edit title, go to editing, clone, delete and download the documents.
               <br /><br />
               Next to each CV is a graphical representation of a PDF to be created. Because of it's size, it's a bit undecipherable, but we believe it's enough for a sneak peak. Once opened, the CV editing view provides a preview with more detail.
           </div>}
-            truncate
-            variant="object-home"
-            onRenderActions={() => {
-              return (
-                <PageHeaderControl>
+          truncate
+          variant="object-home"
+          onRenderActions={() => {
+            return (
+              <PageHeaderControl>
 
-                  <Button
-                    label="Create new CV"
-                    iconCategory="utility"
-                    iconName="add"
-                    iconPosition="left"
-                  />
+                <Button
+                  label="Create new CV"
+                  iconCategory="utility"
+                  iconName="add"
+                  iconPosition="left"
+                />
 
-                </PageHeaderControl>
-              )
-            }
+              </PageHeaderControl>
+            )
+          }
 
-            }
-          />
+          }
+        />
+        <div class="slds-grid slds-gutters cvs slds-wrap ">
+          <div class="slds-col slds-size_3-of-12">
+            <CV header="Lorem ipsum" />
+          </div>
+          <div class="slds-col slds-size_3-of-12">
+            <CV header="Lorem ipsum" />
+
+          </div>
+          <div class="slds-col slds-size_3-of-12">
+            <CV header="Lorem ipsum" />
+
+          </div>
+          <div class="slds-col slds-size_3-of-12">
+            <CV header="Lorem ipsum" />
+
+          </div>
+
         </div>
       </div>
 
