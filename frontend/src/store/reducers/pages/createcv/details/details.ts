afb1f617eb713ecab0c623cfc5aaad66720fd107
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-04 15:19:10 +0100

    Refactor details reducer to use TypeScript.

--- frontend/src/store/reducers/pages/createcv/details/details.ts
+++ frontend/src/store/reducers/pages/createcv/details/details.ts
@@ -1,8 +1,8 @@
-import * as actions from './actionTypes'
+import * as Types from './types'
 
-const initialState = {
+const initialState: Types.DetailsState = {
     header: 'Details',
-    description: 'Various settings related to you. Let the employer know a little more about you!',
+    description: 'Various settings related to you. Let the recruiter know a little more about you!',
     firstName: '',
     middleName: '',
     lastName: '',
@@ -15,63 +15,63 @@ const initialState = {
     birthDate: ''
 }
 
-export default function details(state = initialState, action) {
+export default function details(state: Types.DetailsState = initialState, action: Types.DetailsActionTypes) {
     switch (action.type) {
-        case actions.DETAILS_UPDATE_FIRST_NAME: {
+        case Types.DETAILS_UPDATE_FIRST_NAME: {
             return {
                 ...state,
                 firstName: action.payload.firstName
             }
         }
-        case actions.DETAILS_UPDATE_MIDDLE_NAME: {
+        case Types.DETAILS_UPDATE_MIDDLE_NAME: {
             return {
                 ...state,
                 middleName: action.payload.middleName
             }
         }
-        case actions.DETAILS_UPDATE_LAST_NAME: {
+        case Types.DETAILS_UPDATE_LAST_NAME: {
             return {
                 ...state,
                 lastName: action.payload.lastName
             }
         }
-        case actions.DETAILS_UPDATE_POSITION: {
+        case Types.DETAILS_UPDATE_POSITION: {
             return {
                 ...state,
                 position: action.payload.position
             }
         }
-        case actions.DETAILS_UPDATE_EMAIL: {
+        case Types.DETAILS_UPDATE_EMAIL: {
             return {
                 ...state,
                 email: action.payload.email
             }
         }
-        case actions.DETAILS_UPDATE_MOBILE: {
+        case Types.DETAILS_UPDATE_MOBILE: {
             return {
                 ...state,
                 mobile: action.payload.mobile
             }
         }
-        case actions.DETAILS_UPDATE_COUNTRY: {
+        case Types.DETAILS_UPDATE_COUNTRY: {
             return {
                 ...state,
                 country: action.payload.country
             }
         }
-        case actions.DETAILS_UPDATE_CITY: {
+        case Types.DETAILS_UPDATE_CITY: {
             return {
                 ...state,
                 city: action.payload.city
             }
         }
-        case actions.DETAILS_UPDATE_DRIVING_LICENSE : {
+        case Types.DETAILS_UPDATE_DRIVING_LICENSE: {
             return {
                 ...state,
                 drivingLicense: action.payload.drivingLicense
             }
         }
-        case actions.DETAILS_UPDATE_BIRTH_DATE: {
+        case Types.DETAILS_UPDATE_BIRTH_DATE: {
             return {
                 ...state,
                 birthDate: action.payload.birthDate
