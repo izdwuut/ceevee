330bcd8e19e9353648df4c706ffa6f94cc477485
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-12 12:17:06 +0100

    Add user profile backend.

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,9 +1,18 @@
 from fastapi_users import models
 from fastapi_users.db import TortoiseBaseUserModel, TortoiseUserDatabase
+from typing import Optional
+from datetime import date
 
 
 class User(models.BaseUser):
-    pass
+    first_name: str
+    middle_name: Optional[str]
+    last_name: str
+    mobile: Optional[str]
+    country: Optional[str]
+    city: Optional[str]
+    driving_license: Optional[str]
+    birth_date: date
 
 
 class UserCreate(models.BaseUserCreate):
