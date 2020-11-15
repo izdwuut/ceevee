dafaf5af7dfdc51e6745ed6316ba6bd7f2277db6
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-15 15:14:50 +0100

    Update exprience Pydantic models.

--- backend/routers/cv/experience.py
+++ backend/routers/cv/experience.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
-from models.cv.experience import ExperienceModel, Experience_Pydantic, Experience_Update_Pydantic
+from models.cv.experience import \
+    ExperienceModel, \
+    Experience_Update_In_Pydantic, \
+    Experience_Create_Out_Pydantic, \
+    Experience_Update_Out_Pydantic
 from pydantic import UUID4
 
 
@@ -8,11 +12,10 @@ PREFIX = '/experience'
 
 
 @experience_router.patch(PREFIX + "/{experience_id}")
-async def patch_experience(experience_id: UUID4, experience: Experience_Update_Pydantic):
+async def patch_experience(experience_id: UUID4, experience: Experience_Update_In_Pydantic):
     experience_orm = await ExperienceModel.get(id=experience_id)
-    await experience_orm.update_from_dict(experience.dict(exclude_unset=True))
-    await experience_orm.save()
-    return await Experience_Update_Pydantic.from_tortoise_orm(experience_orm)
+    await experience_orm.update_from_dict(experience.dict(exclude_unset=True)).save()
+    return await Experience_Update_Out_Pydantic.from_tortoise_orm(experience_orm)
 
 
 @experience_router.delete(PREFIX + '/{experience_id}')
@@ -20,8 +23,7 @@ async def delete_experience(experience_id: UUID4):
     await ExperienceModel.get(id=experience_id).delete()
 
 
-@experience_router.post("/{cv_id}/experience", tags=['experience'])
-async def add_experience(cv_id: UUID4, experience: Experience_Pydantic):
+@experience_router.post("/{cv_id}/experience")
+async def add_experience(cv_id: UUID4):
     experience_orm: ExperienceModel = await ExperienceModel.create(cv_id=cv_id)
-    await experience_orm.update_from_dict(experience.dict()).save()
-    return await Experience_Pydantic.from_tortoise_orm(experience_orm)
+    return await Experience_Create_Out_Pydantic.from_tortoise_orm(experience_orm)
