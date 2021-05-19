12a34c04820c416ad2d50ce38540056ec4034581
xrbiay <xrbiay@gmail.com>
2021-05-19 16:00:59 +0200

    finally got pdf's working

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require("electron");
 const puppeteer = require('puppeteer');
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
 
 function createMain() {
     win = new BrowserWindow({
@@ -14,6 +17,33 @@ function createMain() {
     })
     
     win.loadFile("index.html");
+
+    // generate pdf
+    win.webContents.on('did-finish-load', () => {
+
+        var d = new Date();
+        var m = d.getMinutes();
+        var s = d.getSeconds();
+
+        const options = {
+            marginsType: 1,
+            printBackground: false,
+            printSelectionOnly: false,
+            landscape: false,
+            pageSize: 'A4',
+            scaleFactor: 100
+        }
+        const pdfPath = path.join("./pdf-test/", `temp${m}-${s}.pdf`)
+        win.webContents.printToPDF(options).then(data => {
+            console.log(pdfPath);
+            fs.writeFile(pdfPath, data, (error) => {
+            if (error) throw error
+                console.log(`Wrote PDF successfully to ${pdfPath}`)
+            })
+        }).catch(error => {
+                console.log(`Failed to write PDF to ${pdfPath}: `, error)
+            })
+        })
 }
 
 app.on('ready', function () {
